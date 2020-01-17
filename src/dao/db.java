465310aab404956e684a2b0c4ce1e0115ac267d8
kyle-cloud <1139467635@qq.com>
2020-01-17 12:06:23 +0800

    aa

--- src/dao/db.java
+++ src/dao/db.java
@@ -4,5 +4,13 @@ import com.mongodb.DB;
 import com.mongodb.MongoClient;
 
 public class db {
-	
+	public static void main(String[] args) throws Exception {
+		MongoClient client = new MongoClient("localhost", 27017);
+		
+		DB db = client.getDB("test");
+		for(String name : db.getCollectionNames()) {
+			System.out.println("¼¯ºÏÃû×Ö" + name);
+		}
+		client.close();
+	}
 }
