bcc21869902388236d4d5284b2f0de61f235c12e
kyle-cloud <1139467635@qq.com>
2020-02-04 18:52:54 +0800

    实现了细粒度降维，等待测试

--- src/calculation/calculation.java
+++ src/calculation/calculation.java
@@ -3,15 +3,6 @@ package calculation;
 import java.util.ArrayList;
 import java.util.Date;
 
-import com.sun.corba.se.impl.protocol.MinimalServantCacheLocalCRDImpl;
-import com.sun.javafx.collections.MappingChange.Map;
-import com.sun.org.apache.xpath.internal.operations.And;
-import com.sun.xml.internal.bind.v2.runtime.unmarshaller.XsiNilLoader.Array;
-
-import javafx.geometry.Pos;
-import jdk.nashorn.internal.codegen.MethodEmitter;
-import sun.awt.windows.WPrinterJob;
-import sun.net.TelnetOutputStream;
 import trail.Point;
 import trail.Trail;
 
@@ -49,20 +40,24 @@ public class calculation {
 	 *
 	 *细粒度降维
 	 */
-	public ArrayList<ArrayList<Object>> fineCompress(ArrayList<Trail> trail, Integer l, Integer lambda) {
-		ArrayList<ArrayList<Object>> result = null;
-		ArrayList<Point> minTra = null;
-		Point minp = null;
+	public ArrayList<Object> fineCompress(ArrayList<Trail> trail, Integer l, Integer lambda) {
+		ArrayList<Object> result = new ArrayList<Object>();
+		ArrayList<Trail> finTra = new ArrayList<Trail>(); //最终轨迹
+		ArrayList<Point> tmpTra = new ArrayList<Point>(); //子轨迹降维之后
+		ArrayList<Integer> fSum = new ArrayList<Integer>(); //最终轨迹每段点个数
+		int fsum = 0; //子轨迹降维后的点个数
+		ArrayList<Point> minTra = new ArrayList<Point>();
+		Point minp = new Point();
 		for(int i = 0; i < trail.size(); i ++) {
 			Trail subTra = trail.get(i);
-			minTra.add(subTra.getPoints().get(0));
 			ArrayList<Point> points = subTra.getPoints();
+			minTra.add(subTra.getPoints().get(0)); //存储每一个分段
 			for(int j = 1; j < points.size(); j ++) {
 				if(calcDistance(points.get(j), minTra.get(0)) >= l || calcDistOfDate(points.get(j), minTra.get(0)) >= lambda) {
 					double sumTime = calcDistOfDate(minTra.get(0), minTra.get(minTra.size()-1));
 					//求权重
-					ArrayList<Double> wp = null;
-					ArrayList<Point> qc_points = null;
+					ArrayList<Double> wp = new ArrayList<Double>();
+					ArrayList<Point> qc_points = new ArrayList<Point>();
 					for(int k = 0; k < minTra.size(); k ++) {
 						int stay = k;
 						for(int h = k; h < minTra.size(); h ++) {//查看最后停留在此点的时间
@@ -92,16 +87,36 @@ public class calculation {
 						qc_points.add(minTra.get(k));
 					}
 					//求聚合点
+					double minp_lng = 0;
+					double minp_lat = 0;
+					double minp_wp = 0;
 					int qc_sum = qc_points.size();
-					
+					for(int k = 0; k < qc_sum; k ++) {
+						minp_lng += qc_points.get(k).getLng() * wp.get(k);
+						minp_lat += qc_points.get(k).getLat() * wp.get(k);
+						minp_wp += wp.get(k);
+					}
+					minp.setLng(minp_lng / minp_wp);
+					minp.setLat(minp_lat / minp_wp);
+					minp.setDate(meanDate(minTra.get(0).getDate(), minTra.get(minTra.size()-1).getDate()));
+					tmpTra.add(minp);
+					fsum ++;
 					
 					minTra.clear();
 					minTra.add(points.get(j));
 				} else {
-					minTra.add(points.get(i));
+					minTra.add(points.get(j));
 				}
 			}
+			Trail tmpTrail = new Trail();
+			tmpTrail.setPoints(tmpTra);
+			finTra.add(tmpTrail);
+			fSum.add(fsum);
+			tmpTra.clear();
+			fsum = 0;
 		}
+		result.add(finTra);
+		result.add(fsum);
 		return result;
 	}
 	
