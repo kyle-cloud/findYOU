999b2f0036870053bc31e1e5a4c5cbcab3ca5bce
kyle-cloud <1139467635@qq.com>
2020-02-21 17:41:45 +0800

    将时间存储为字符串，等拿出来再转格式，不然进去转一次（数据库里拿出来是object），出来还要转一次

--- src/process/processData.java
+++ src/process/processData.java
@@ -1,40 +1,37 @@
 package process;
 
-import java.awt.datatransfer.StringSelection;
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
-import java.sql.Date;
 import java.text.DateFormat;
+import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
-import java.util.List;
+import java.util.Date;
 
 import org.bson.Document;
-import org.eclipse.jdt.internal.compiler.codegen.BranchLabel;
 
 import com.mongodb.BasicDBObject;
-import com.mongodb.DBCollection;
 import com.mongodb.client.MongoCollection;
+import com.mongodb.client.MongoCursor;
 
 import dao.MongoUtil;
 
 public class processData {
-	DateFormat format = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+	static DateFormat format = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
 	
-	public void readFile(String path) {
+	public static void readFile(String path) {
 		File file = new File(path);
 		BufferedReader bReader = null;
-		ArrayList<Date> dates = new ArrayList<>();
+		ArrayList<String> dates = new ArrayList<>();
 		ArrayList<Double> longitudes = new ArrayList<>();
 		ArrayList<Double> latitudes = new ArrayList<>();
 		try {
 			bReader = new BufferedReader(new FileReader(file));
 			for(String line = new String(bReader.readLine().getBytes(), "utf-8"); line != null; line = bReader.readLine()) {
-				BasicDBObject basicDBObject = new BasicDBObject();
 				String[] strs = line.split(",");
-				dates.add((Date) format.parse(strs[1]));
+				dates.add(strs[1]);
 				longitudes.add(Double.parseDouble(strs[2]));
 				latitudes.add(Double.parseDouble(strs[3]));
 			}
@@ -49,10 +46,24 @@ public class processData {
 		} finally {
 			try {
 				bReader.close();
-				MongoUtil.instance.close();
 			} catch (IOException e) {
 				e.printStackTrace();
 			}
 		}
+	}
+	
+	@SuppressWarnings("deprecation")
+	public static void main(String[] args) throws Exception {
+		readFile("D:\\Trail_Data\\taxi_log_2008_by_id\\1.txt");
+		
+//		查看时间格式		
+//		MongoCollection<Document> coll = MongoUtil.instance.getCollection("liu", "trail");
+//		MongoCursor<Document> sd =coll.find().iterator();
+//        while(sd.hasNext()) {
+//        	Document doc = sd.next();
+//        	ArrayList<String> result = (ArrayList<String>)doc.get("Date");
+//        	System.out.println((Date)format.parse(result.get(0)));
+//        }
+        MongoUtil.instance.close();
 	}
 }
