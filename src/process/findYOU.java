e79a53bd391789c212f6f3b5bf5efe60d581320b
kyle-cloud <1139467635@qq.com>
2020-02-26 14:31:03 +0800

    直接存入轨迹到数据库

--- src/process/findYOU.java
+++ src/process/findYOU.java
@@ -3,11 +3,11 @@ package process;
 import java.util.ArrayList;
 
 import org.bson.Document;
+
 import com.mongodb.client.MongoCollection;
 
 import calculation.calculations;
 import dao.MongoUtil;
-import net.sf.json.JSONArray;
 import trail.Point;
 import trail.Trail;
 
@@ -15,6 +15,7 @@ public class findYOU {
 	public static void main(String[] args){
 		ArrayList<Trail> dividedTrail = new ArrayList<>();
 		ArrayList<Point> coarseTrail = new ArrayList<>();
+		ArrayList<Trail> finTrails = new ArrayList<>();
 		try{
 //			MongoUtil.instance.dropCollection("liu", "coarseTrail");
 			//获取轨迹数据
@@ -24,11 +25,19 @@ public class findYOU {
 			for(int i = 0; i < trails.size(); i ++) {
 				dividedTrail = calculations.divideTrace(trails.get(i), 60*60*1000);
 				coarseTrail = calculations.coarseCompress(dividedTrail);
-				Document document = new Document();
-				JSONArray jsonArray = JSONArray.fromObject(coarseTrail);
-				document.put("Trail", jsonArray);
-				MongoCollection<Document> coll = MongoUtil.instance.getCollection("liu", "coarseTrail");
-				coll.insertOne(document);
+				Trail finTrail = new Trail();
+				finTrail.setIMSI(trails.get(i).getIMSI());
+				finTrail.setPoints(coarseTrail);
+				finTrail.setSum_points(coarseTrail.size());
+				finTrail.setTstart(coarseTrail.get(0).getDate());
+				finTrail.setTend(coarseTrail.get(coarseTrail.size()-1).getDate());
+				finTrails.add(finTrail);
+				
+//				Document document = new Document();
+//				net.sf.json.JSONObject jObject = net.sf.json.JSONObject.fromObject(finTrail);
+//				document.put("Trail", jObject);
+//				MongoCollection<Document> coll = MongoUtil.instance.getCollection("liu", "coarseTrail");
+//				coll.insertOne(document);
 			}
 		} catch (Exception e) {
 			e.printStackTrace();
