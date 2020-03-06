ef5b9701118360368546f046cc59fd8b4fa6757c
kyle-cloud <1139467635@qq.com>
2020-03-06 16:03:18 +0800

    ok,关于时间分段大小测试，决定先取420比较合适，具体见参考论文侧图

--- src/test/test.java
+++ src/test/test.java
@@ -1,7 +1,43 @@
 package test;
 
-import dao.MongoUtil;
+import java.util.ArrayList;
+import java.util.HashMap;
+
+import org.apache.commons.collections.map.HashedMap;
+
+import com.sun.corba.se.impl.oa.poa.ActiveObjectMap.Key;
+import com.sun.javafx.collections.MappingChange.Map;
+
+import calculation.calculations;
+import process.downloadData;
+import trail.Trail;
 
 public class test {
+	public static void testTimeSegment() {
+		HashMap<Double, Integer> map = new HashMap<Double, Integer>();
+		ArrayList<Trail> trails = downloadData.getTrails();
+		for(int i = 0; i < trails.size(); i ++) {
+			ArrayList<Trail> dividedTrail = calculations.divideTrace(trails.get(i), 420*60*1000);
+			int sum = 0;
+			for(int j = 0; j < dividedTrail.size(); j ++) {
+				if(dividedTrail.get(j).getSum_points() >= 10) {
+					sum ++;
+				}
+			}
+			double percent = (double)sum / (double)dividedTrail.size();
+			double key = (double)Math.round(percent * 10) / 10;
+			if(map.containsKey(key)) {
+				map.put(key, map.get(key) + 1);
+			} else {
+				map.put(key, 1);
+			}
+		}
+		map.forEach((k, v) ->
+			System.out.println(k + ":" + v)
+		);
+	}
 	
+	public static void main(String[] args) {
+		testTimeSegment();
+	}
 }
